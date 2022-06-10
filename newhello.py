3da1ecd23694a3f75ef47f760f62d66187d57464
althaf <althaf.sharafudheen@outlook.com>
2022-06-10 20:47:26 +0400

    addition two numbers

--- newhello.py
+++ newhello.py
@@ -1,3 +1,6 @@
 a = input('enter 2 number')
 b = input()
-print("hello world" + a + "second value is " + b )
+print("hello world" + a + "second value is " + b)
+
+sum = (int(a) + int(b))
+print("result is " + str(sum))
