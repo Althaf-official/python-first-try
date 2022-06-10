d5aaf292836fa2dccb8b62ba1ed074cfb31fa038
althaf <althaf.sharafudheen@outlook.com>
2022-06-10 21:14:59 +0400

    multiline comment

--- swaping.py
+++ swaping.py
@@ -9,4 +9,11 @@ t=a
 a=b
 b=t
 #this is sample comment
+""""this is multiline com
+
+
+
+dsalkjfl
+dsf;klakdlkj
+lkjfaljd;aklment"""
 print("first number is " + str(a) + "second number is  "+str(b))
