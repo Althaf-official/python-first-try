d7a737ce8cc06f64b915261e6f2ff8337629372f
althaf <althaf.sharafudheen@outlook.com>
2022-06-12 18:08:03 +0400

    else if or elif

--- if sample.py
+++ if sample.py
@@ -2,5 +2,12 @@ num=int(input("enter a number"))
 
 if num<0:
     print("entered number is negative")
+
+elif num ==0:
+    print("entered number is zero")
+
+elif num ==1:
+    print("entered value is one")
+
 else:
     print("entered number is positive")
