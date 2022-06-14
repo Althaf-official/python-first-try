8716dbff274176500b7b80f6d34131184a88d22f
althaf <althaf.sharafudheen@outlook.com>
2022-06-14 18:45:28 +0400

    function called in a different page

---
+++ using module(call the mofule).py
@@ -0,0 +1,5 @@
+import using_module
+
+
+using_module.checkNegorPos(9)
+
---
+++ using_module.py
@@ -0,0 +1,12 @@
+def checkNegorPos(num):
+    if num < 0:
+        print("entered number is negative")
+
+    elif num == 0:
+        print("entered number is zero")
+
+    elif num == 1:
+        print("entered value is one")
+
+    else:
+        print("entered number is positive")
