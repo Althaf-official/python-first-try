ad8c4f6a0d3d568931fffb316efa58a0b5ed3b62
althaf <althaf.sharafudheen@outlook.com>
2022-06-14 19:07:25 +0400

    import function  from a module as a mascheck

--- module.py
+++ module.py
@@ -1,5 +1,10 @@
 #from using_module import checkNegorPos
 #checkNegorPos(12)
 
-import using_module as mas
-mas.checkNegorPos(15)
+#import using_module as mas
+#mas.checkNegorPos(15)\
+
+
+from using_module import checkNegorPos as mascheck
+mascheck(10)
+mascheck(-12)
