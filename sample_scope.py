af7b81da64ea4c8826089aa76365ba0d0135777d
althaf <althaf.sharafudheen@outlook.com>
2022-06-15 20:59:43 +0400

    scope

---
+++ sample_scope.py
@@ -0,0 +1,15 @@
+#pyhton is object oriented ,funcional oriented and procedure oriented programming
+# in python we can define a function inside a function. in node js we can call a function we cannot define
+def check_scope():
+    def do_local():
+        test="local test"
+    def do_non_local():
+        test="non local test"
+    def do_global():
+        test="global test"
+
+    test="default"#this test is working in this case
+    do_local()
+    print("test value after do local: "+test)
+
+check_scope()
