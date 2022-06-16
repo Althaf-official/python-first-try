dc1d25a22bf23ab34c590e9e5778f31e97064250
althaf <althaf.sharafudheen@outlook.com>
2022-06-16 19:10:28 +0400

    scope

--- sample_scope.py
+++ sample_scope.py
@@ -7,6 +7,7 @@ def check_scope():
         nonlocal test
         test="non local test"
     def do_global():
+        global test
         test="global test"
 
     test="default"#this test is working in this case
@@ -14,5 +15,8 @@ def check_scope():
     print("test value after do local: "+test)
     do_non_local()
     print("test value after do non local: " + test)
+    do_global()
+    print("test value after do global: " + test)
 
 check_scope()
+print("text value after main  :"+test)
