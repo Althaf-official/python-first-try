680921a2a50e3b486dab78476b48c8314439d418
althaf <althaf.sharafudheen@outlook.com>
2022-06-16 19:27:22 +0400

    class sample pass a aurgument

--- classsample.py
+++ classsample.py
@@ -1,5 +1,6 @@
 class MySampleClass: #this is a class
-    def hello(self):  #this is a method /function
-        print("hello")
-c=MySampleClass()#we call them here
-MySampleClass.hello(c)
+    def hello(self,o):  #this is a method /function
+        print("hello "+o)
+c=MySampleClass()#we assaign MysampleClass as a varibale
+name="althaf"
+c.hello(name) #we pass the variable here
