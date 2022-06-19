5907b42f9c798b644e41c7a6e4f59493a6cbb096
althaf <althaf.sharafudheen@outlook.com>
2022-06-19 18:02:37 +0400

    multiple inheritance - some description as a comment

--- multiple_inheritance_sample.py
+++ multiple_inheritance_sample.py
@@ -2,13 +2,13 @@ class First:
     def display_first(self):
         print("first")
 
-class Second(First):
+class Second(First):#inheritted from the first class
     def display_second(self):
         print("second")
 
 #now display_first and display_second  going to inherit to another c
 
-class Third(Second):
+class Third(Second):#inherit from the sencond class
     def display_third(self):
         print("third")
 
