d54cbca9c42db84fad66145c7a6f25faa5e6f93d
althaf <althaf.sharafudheen@outlook.com>
2022-06-19 17:58:23 +0400

    multiple inheritance - created same name function in different classes and pass the class name a as a aurgument in the each class name

--- multiple_inheritance_sample.py
+++ multiple_inheritance_sample.py
@@ -1,18 +1,20 @@
 class First:
-    def display(self):
+    def display_first(self):
         print("first")
 
-class Second:
-    def display(self):
+class Second(First):
+    def display_second(self):
         print("second")
 
 #now display_first and display_second  going to inherit to another c
 
-class Third(First,Second):
+class Third(Second):
     def display_third(self):
         print("third")
 
 
 x=Third()
 x.display_third()
-x.display()
+x.display_first()
+x.display_second()
+
