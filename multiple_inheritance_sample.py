d719bc36f91a63fe4d6ce85a97c28807be4a869f
althaf <althaf.sharafudheen@outlook.com>
2022-06-19 18:10:43 +0400

    multiple inheritance - how to find  order

--- multiple_inheritance_sample.py
+++ multiple_inheritance_sample.py
@@ -6,7 +6,7 @@ class Second():
     def display(self):
         print("second")
 
-class Third(First,Second):#(First,Second))  it will show the result from left to right .means if you make the argument second it will show the function second firstly
+class Third(Second,First):#(First,Second))  it will show the result from left to right .means if you make the argument second it will show the function second firstly
     def display1(self):
         print("third")
 #this case it will only search until found.
@@ -15,4 +15,6 @@ x=Third()
 x.display()
 
 print(Third.mro())
+
+#now the Class name Second comes first
 
